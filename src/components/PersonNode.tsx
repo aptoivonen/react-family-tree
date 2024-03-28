2b9190364a9b212d55eeb77b57aec0f66e264f32
Aleksi Toivonen <aptoivonen@gmail.com>
2024-03-28 17:30:49 +0200

    refactor: create person node wrapper

--- src/components/PersonNode.tsx
+++ src/components/PersonNode.tsx
@@ -1,13 +1,11 @@
+import PersonNodeWrapper from './PersonNodeWrapper';
 import type { ExtNode, Profile } from '@/types';
-
-import { NODE_HEIGHT, NODE_WIDTH } from '@/constants';
 
 type Props = {
   node: ExtNode;
   profile: Profile;
 };
 
-const nodeGap = 'p-[20px]';
 const maleColor = 'border-[#17B4CE]';
 const femaleColor = 'border-[#FC8181]';
 
@@ -15,22 +13,13 @@ export default function FamilyNode({ node, profile }: Props) {
   const borderColor = node.gender === 'male' ? maleColor : femaleColor;
 
   return (
-    <div
-      className={`absolute ${nodeGap}`}
-      style={{
-        width: NODE_WIDTH,
-        height: NODE_HEIGHT,
-        transform: `translate(${node.left * (NODE_WIDTH / 2)}px, ${
-          node.top * (NODE_HEIGHT / 2)
-        }px)`,
-      }}
-    >
+    <PersonNodeWrapper top={node.top} left={node.left}>
       <div
         className={`grid justify-items-center h-full bg-[#fffffe] border-2 rounded-xl ${borderColor}`}
       >
         <div className="text-xs">{node.id}</div>
         <div className="text-xl font-bold">{profile.name}</div>
       </div>
-    </div>
+    </PersonNodeWrapper>
   );
 }
---
+++ src/components/PersonNodeWrapper.tsx
@@ -0,0 +1,28 @@
+import { NODE_HEIGHT, NODE_WIDTH } from '@/constants';
+
+const nodeGap = 'p-[20px]';
+
+type Props = {
+  children: React.ReactNode;
+  left: number;
+  top: number;
+};
+
+function PersonNodeWrapper({ children, left, top }: Props) {
+  return (
+    <div
+      className={`absolute ${nodeGap}`}
+      style={{
+        width: NODE_WIDTH,
+        height: NODE_HEIGHT,
+        transform: `translate(${left * (NODE_WIDTH / 2)}px, ${
+          top * (NODE_HEIGHT / 2)
+        }px)`,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default PersonNodeWrapper;
