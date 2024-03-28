ca36d38da42a816fbf0ebc42d2be0309a6c99891
Aleksi Toivonen <aptoivonen@gmail.com>
2024-03-28 16:22:22 +0200

    refactor: clean files

--- src/components/PersonNode.tsx
+++ src/components/PersonNode.tsx
@@ -1,40 +1,35 @@
 import type { ExtNode, Profile } from '@/types';
 
-import { NODE_HEIGHT, NODE_WIDTH, NODE_DISTANCE } from '@/constants';
+import { NODE_HEIGHT, NODE_WIDTH } from '@/constants';
 
 type Props = {
   node: ExtNode;
   profile: Profile;
 };
 
+const nodeGap = 'p-[20px]';
+const maleColor = 'border-[#17B4CE]';
+const femaleColor = 'border-[#FC8181]';
+
 export default function FamilyNode({ node, profile }: Props) {
-  const borderColor = node.gender === 'male' ? 'blue' : 'pink';
+  const borderColor = node.gender === 'male' ? maleColor : femaleColor;
 
   return (
     <div
+      className={`absolute ${nodeGap}`}
       style={{
-        position: 'absolute',
         width: NODE_WIDTH,
         height: NODE_HEIGHT,
-        padding: `${NODE_DISTANCE / 2}px`,
         transform: `translate(${node.left * (NODE_WIDTH / 2)}px, ${
           node.top * (NODE_HEIGHT / 2)
         }px)`,
       }}
     >
       <div
-        style={{
-          display: 'grid',
-          justifyItems: 'center',
-          height: '100%',
-          border: `2px solid ${borderColor}`,
-          backgroundColor: 'white',
-        }}
+        className={`grid justify-items-center h-full bg-[#fffffe] border-2 rounded-xl ${borderColor}`}
       >
-        <div style={{ fontSize: '0.7rem' }}>{node.id}</div>
-        <div style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
-          {profile.name}
-        </div>
+        <div className="text-xs">{node.id}</div>
+        <div className="text-xl font-bold">{profile.name}</div>
       </div>
     </div>
   );
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
 export const NODE_WIDTH = 170;
 export const NODE_HEIGHT = 100;
-export const NODE_DISTANCE = 40;
