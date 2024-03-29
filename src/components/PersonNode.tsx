9536c84c6e95a142fc7c7803c7ab502c606bb4c6
Aleksi Toivonen <aptoivonen@gmail.com>
2024-03-29 09:51:31 +0200

    feat: improve person node

--- src/components/PersonNode.tsx
+++ src/components/PersonNode.tsx
@@ -16,10 +16,11 @@ export default function FamilyNode({ node, profile }: Props) {
   return (
     <PersonNodeWrapper top={node.top} left={node.left}>
       <div
-        className={`grid grid-cols-[1fr_2fr] h-full p-1 rounded-xl ${backgroundColor} border border-[#cccccc] shadow-lg`}
+        className={`grid grid-cols-[1fr_2fr] gap-1 h-full p-1 rounded ${backgroundColor} border border-gray-300 shadow-lg`}
       >
-        <div className="absolute top-0 left-[25px] text-xs">{node.id}</div>
-        <IoIosPerson className="w-full h-full text-gray-500 border-1 border-dashed border-black" />
+        <div className="bg-gray-400 rounded">
+          <IoIosPerson className="w-full h-full text-gray-100" />
+        </div>
         <div className="grid items-center text-xs font-bold leading-none">
           {profile.name}
         </div>
